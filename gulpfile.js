b27db7f6ef396cfc3b16b10187b58623bc6ab7cf
Othniel Visky <viskytofi@yahoo.com>
2016-11-04 19:48:28 +0200

    new gulfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,18 @@
 'use strict'
 
 let gulp = require('gulp'),
-    babel = require('gulp-babel'),
-    concat = require('gulp-concat'),
-    sourceMaps = require('gulp-sourcemaps'),
-    sass = require('gulp-sass'),
-    plumber = require('gulp-plumber'),
-    cleanCss = require('gulp-clean-css')
+	babel = require('gulp-babel'),
+	concat = require('gulp-concat'),
+	sourceMaps = require('gulp-sourcemaps'),
+	sass = require('gulp-sass'),
+	plumber = require('gulp-plumber'),
+	cleanCss = require('gulp-clean-css'),
+	Server = require('karma').Server,
+	eslint = require('gulp-eslint')
 
 
-gulp.task('transpile', () => {
-  return gulp.src('frontend/src/**/*.js')
+gulp.task('transpile', ['lint'], () => {
+	return gulp.src('frontend/src/**/*.js')
              .pipe(plumber())
              .pipe(sourceMaps.init())
              .pipe(babel({ presets: ['latest'] }))
@@ -20,7 +22,7 @@ gulp.task('transpile', () => {
 })
 
 gulp.task('sass', () => {
-  return gulp.src('frontend/styles/styles.scss')
+	return gulp.src('frontend/styles/styles.scss')
              .pipe(plumber())
              .pipe(sourceMaps.init())
              .pipe(sass())
@@ -30,8 +32,23 @@ gulp.task('sass', () => {
 })
 
 gulp.task('watch', () => {
-  gulp.watch('frontend/src/**/*.js', ['transpile'])
-  gulp.watch('frontend/src/**/*.scss', ['sass'])
+	gulp.watch('frontend/src/**/*.js', ['transpile'])
+	gulp.watch('frontend/src/**/*.scss', ['sass'])
+})
+
+gulp.task('unit-test', (done) => {
+	new Server({
+		configFile: __dirname + '/karma.conf.js'
+	}, () => {
+		done()
+	}).start()
+})
+
+gulp.task('lint', () => {
+	return gulp.src(['backend/**/*.js', 'frontend/**/*.js', '!frontend/build/**/*.js', 'test/**/*.js', '!test/coverage/**/*.js'])
+             .pipe(eslint())
+             .pipe(eslint.format())
+             .pipe(eslint.failAfterError())
 })
 
 gulp.task('default', ['sass', 'transpile', 'watch'])
