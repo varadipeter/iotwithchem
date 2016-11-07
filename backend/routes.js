1376729bae69c73fc3bf0899d19b97f49af64d85
zaim1489-exam <zaim1489@cs.ubbcluj.ro>
2016-11-07 20:19:08 +0200

    routes.js modified

--- backend/routes.js
+++ backend/routes.js
@@ -1,47 +1,64 @@
-'use strict'
+  'use strict'
 
-let path = require('path'),
-    mongoose = require('mongoose'),
-    User = require(path.resolve('backend/models/users.js')),
-	Temperature = require(path.resolve('backend/models/temperature.js')),
-	Alive = require(path.resolve('backend/models/alive.js'))
-	
-var tempvalue;
-var temp	
+  let path = require('path'),
+      mongoose = require('mongoose'),
+      User = require(path.resolve('backend/models/users.js')),
+    Temperature = require(path.resolve('backend/models/temperature.js')),
+    Alive = require(path.resolve('backend/models/alive.js'))
+    
+  var tempvalue;
+  var temp	
 
-module.exports = (app) => {
-  app.get('/users', (request, response) => {
-  
-    User.find((error, users) => {
-      if (error) { response.send(error) }
+  module.exports = (app) => {
+    app.get('/users', (request, response) => {
+    
+      User.find((error, users) => {
+        if (error) { response.send(error) }
 
-      response.json(users)
+        response.json(users)
+      })
     })
-  })
-  
-  app.get('/temperatures', (request, response) => {
-  
-    Temperature.find((error, temperatures) => {
-      if (error) { response.send(error) }
+    
 
-      response.json(temperatures[temperatures.length-1])
+    app.get('/getsensorids', (request, response) => {
+      
+      var _sensorids = new Set();
+      Temperature.find([] ,(error, sensors) => {
+        if (error) { response.send(error) }
+        sensors.forEach(function(item) {
+           _sensorids.add(item.sensorid) 
+          })
+        response.json(Array.from(_sensorids));         
+      });
+
+    })  
+
+
+    app.get('/gettemperature', (request, response) => {
+      
+      var sensorid = request.param('sensorid');  
+      if (typeof sensorid === 'undefined') sensorid = '1';
+
+      Temperature.find((error, temperatures) => {
+        if (error) { response.send(error) }
+        response.json(temperatures[temperatures.length-1]);       
+      }).where('sensorid').equals(sensorid);    
+
+    })  
+    
+
+    app.get('/isalive', (request, response) => {
+    
+      Alive.find((error, alivedata) => {
+        if (error) { response.send(error) }
+
+        response.json(alivedata[alivedata.length-1])
+      })
     })
-  })  
-  
-   app.get('/isalive', (request, response) => {
-  
-  var a = new Alive({alivedate:'makos'})
-  
-    Alive.find((error, alivedata) => {
-      if (error) { response.send(error) }
+    
 
-      response.json(alivedata[alivedata.length-1])
-    })
-  })
-  
-
-  app.get('*', (request, response) => {
-    response.sendFile(path.resolve('frontend/index.html'));
-  });
-}
+    app.get('*', (request, response) => {
+      response.sendFile(path.resolve('frontend/index.html'));
+    });
+  }
 
