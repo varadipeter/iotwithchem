09a85cbf12572c527b4d8676992612af29da3b6d
zaim1489-exam <zaim1489@cs.ubbcluj.ro>
2016-11-09 00:42:03 +0200

    verified with eslint

--- backend/pi/pi.js
+++ backend/pi/pi.js
@@ -1,38 +1,38 @@
-var devices = require('./devices/devices');
-var cpuinfo = require('./devices/cpuinfo')();
-var serialNumber = cpuinfo['Serial'] == undefined ? -1 : cpuinfo['Serial'][0];
-var db = require('../models/uploadData');
-var datetime = require('node-datetime');
-var mongoose = require('mongoose');
+var devices = require('./devices/devices')
+var cpuinfo = require('./devices/cpuinfo')()
+var serialNumber = cpuinfo['Serial'] == undefined ? -1 : cpuinfo['Serial'][0]
+var db = require('../models/uploadData')
+var datetime = require('node-datetime')
+var mongoose = require('mongoose')
 
 mongoose.connection.on('error', (error) => {
-  console.log('Could not connect to mongo server!')
-  console.log(error);
+	console.log('Could not connect to mongo server!')
+	console.log(error)
 })
 
 // connect to database on mongolab
 mongoose.connect('mongodb://heroku_hww55rc1:2ic4cjhncvmlse83a21lnejpru@ds139187.mlab.com:39187/heroku_hww55rc1',function(err) {
-if (err) console.log("erros:"+err);
-}); //('mongodb://votiv:votiv@ds031257.mlab.com:31257/kemia-db')
+	if (err) console.log('erros:'+err)
+}) //('mongodb://votiv:votiv@ds031257.mlab.com:31257/kemia-db')
 
 //Prints temperature to console
 function temperatureToConsole(){
 	devices.temperatureDevice(function(err,value){
-			console.log('Raspberry -',serialNumber);
-			console.log('Current temperature on sensor is', value);
-            db.createTemperatureMessage(serialNumber,'1',value,new Date().getTime(),function(err){
-            if (err) console.log(err);            
-            });
-		});
+		console.log('Raspberry -',serialNumber)
+		console.log('Current temperature on sensor is', value)
+		db.createTemperatureMessage(serialNumber,'1',value,new Date().getTime(),function(err){
+			if (err) console.log(err)            
+		})
+	})
 }
 
 function IsAlive(){
-var d = new Date().getTime();
-db.createAliveMessage(serialNumber,d,function(err){
-            if (err) console.log(err);            
-            });
-console.log('Alive -',d);
+	var d = new Date().getTime()
+	db.createAliveMessage(serialNumber,d,function(err){
+		if (err) console.log(err)            
+	})
+	console.log('Alive -',d)
 }
-setInterval(IsAlive,3000);
+setInterval(IsAlive,3000)
 
-tempIntervalID = setInterval(temperatureToConsole,4000);
+tempIntervalID = setInterval(temperatureToConsole,4000)
--- backend/routes.js
+++ backend/routes.js
@@ -1,61 +1,61 @@
-  'use strict'
+'use strict'
 
-  let path = require('path'),
-      mongoose = require('mongoose'),      
-      db = require(path.resolve('backend/models/downloadData.js'))
+let path = require('path'),
+	mongoose = require('mongoose'),      
+	db = require(path.resolve('backend/models/downloadData.js'))
     
-  var raspiAlive = false;
+var raspiAlive = false
 
-  module.exports = (app) => {
+module.exports = (app) => {
     
-    app.get('/getsensorids', (request, response) => {
+	app.get('/getsensorids', (request, response) => {
 
-        db.getTemperatureSensors(function(returndata){
-          response.json(returndata);
-        });
-    })  
-
-
-    app.get('/gettemperature', (request, response) => {  
-
-        var sensorid = request.param('sensorid');  
-        if (typeof sensorid === 'undefined') sensorid = '1';
-
-        db.getTemperature(sensorid,function(returndata){
-          response.json(returndata);
-        });
-    })
+		db.getTemperatureSensors(function(returndata){
+			response.json(returndata)
+		})
+	})  
 
 
-    app.get('/gettemperatureinterval', (request, response) => {
+	app.get('/gettemperature', (request, response) => {  
+
+		var sensorid = request.param('sensorid')  
+		if (typeof sensorid === 'undefined') sensorid = '1'
+
+		db.getTemperature(sensorid,function(returndata){
+			response.json(returndata)
+		})
+	})
+
+
+	app.get('/gettemperatureinterval', (request, response) => {
       
-      var sensorid = request.param('sensorid');
-      var datefrom = request.param('datefrom');
-      var dateto = request.param('dateto');  
-      if (typeof sensorid === 'undefined') sensorid = '1';
+		var sensorid = request.param('sensorid')
+		var datefrom = request.param('datefrom')
+		var dateto = request.param('dateto')  
+		if (typeof sensorid === 'undefined') sensorid = '1'
 
-      db.getTemperatureInterval(sensorid,datefrom,dateto,function(returndata){
-          response.json(returndata);
-      });
+		db.getTemperatureInterval(sensorid,datefrom,dateto,function(returndata){
+			response.json(returndata)
+		})
 
-    })      
+	})      
 
 
-    app.get('/isalive', (request, response) => {
+	app.get('/isalive', (request, response) => {
 
-        response.json({alive : raspiAlive})
-    })
+		response.json({alive : raspiAlive})
+	})
     
 
-    app.get('*', (request, response) => {
+	app.get('*', (request, response) => {
      
-      response.sendFile(path.resolve('frontend/index.html'));
-    });
+		response.sendFile(path.resolve('frontend/index.html'))
+	})
 
-  }
+}
 
 
 
-  setInterval(function () { db.getPulse(function(returndata){
-    raspiAlive = returndata
-  });},5000);
+setInterval(function () { db.getPulse(function(returndata){
+	raspiAlive = returndata
+})},5000)
