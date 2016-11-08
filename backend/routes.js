3853098255635819981a47d8dad06a2ee3fe4ada
zaim1489-exam <zaim1489@cs.ubbcluj.ro>
2016-11-09 00:09:47 +0200

    route.js modified and added downloadData.js *get methods inplemented for ajax requests for get all data about temperatures and sensors *donwloadData model to get Data from mongo db

--- backend/routes.js
+++ backend/routes.js
@@ -2,70 +2,60 @@
 
   let path = require('path'),
       mongoose = require('mongoose'),      
-    Temperature = require(path.resolve('backend/models/temperature.js')),
-    Alive = require(path.resolve('backend/models/alive.js'))
+      db = require(path.resolve('backend/models/downloadData.js'))
     
-  var tempvalue;
-  var temp;
-  var lastAliveDate = 0;
   var raspiAlive = false;
 
   module.exports = (app) => {
     
     app.get('/getsensorids', (request, response) => {
-      
-      var _sensorids = new Set();
-      Temperature.find({},'-_id -__v' ,(error, sensors) => {
-        if (error) { response.send(error) }
-        sensors.forEach(function(item) {
-           _sensorids.add(item.sensorid) 
-          })
-        response.json(Array.from(_sensorids));         
-      });
 
+        db.getTemperatureSensors(function(returndata){
+          response.json(returndata);
+        });
     })  
 
 
-    app.get('/gettemperature', (request, response) => {
+    app.get('/gettemperature', (request, response) => {  
+
+        var sensorid = request.param('sensorid');  
+        if (typeof sensorid === 'undefined') sensorid = '1';
+
+        db.getTemperature(sensorid,function(returndata){
+          response.json(returndata);
+        });
+    })
+
+
+    app.get('/gettemperatureinterval', (request, response) => {
       
-      var sensorid = request.param('sensorid');  
+      var sensorid = request.param('sensorid');
+      var datefrom = request.param('datefrom');
+      var dateto = request.param('dateto');  
       if (typeof sensorid === 'undefined') sensorid = '1';
 
-      Temperature.find({},'-_id -__v',(error, temperatures) => {
-        if (error) { response.send(error) }
-        response.json(temperatures[temperatures.length-1]);       
-      }).where('sensorid').equals(sensorid);    
+      db.getTemperatureInterval(sensorid,datefrom,dateto,function(returndata){
+          response.json(returndata);
+      });
 
-    })  
-    
+    })      
+
 
     app.get('/isalive', (request, response) => {
-    
-      Alive.find((error, alivedata) => {
-        if (error) { response.send(error) }
-        var respJson = JSON.parse('{"alive":'+raspiAlive+'}');
+
         response.json({alive : raspiAlive})
-      })
     })
     
 
     app.get('*', (request, response) => {
+     
       response.sendFile(path.resolve('frontend/index.html'));
     });
+
   }
 
-function pulseMeter(){
-  Alive.find((error, alivedata) => {
-    var currentLastDate = alivedata[alivedata.length-1].alivedate;
-      console.log('raspiAlive-',raspiAlive);
-      console.log(currentLastDate,lastAliveDate);
-    if(lastAliveDate != 0 && lastAliveDate != currentLastDate){
-      raspiAlive = true;
-    }else{
-      raspiAlive = false;
-    }
-    lastAliveDate = currentLastDate;
-  });
-}
 
-setInterval(function () { pulseMeter();},5000);
+
+  setInterval(function () { db.getPulse(function(returndata){
+    raspiAlive = returndata
+  });},5000);
