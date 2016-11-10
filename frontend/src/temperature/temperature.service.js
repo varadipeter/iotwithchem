99a2481904ac8b2f99cb971d7395ed753035570f
Matyas Robert Janos <matyas.robi@yahoo.com>
2016-11-10 02:21:10 +0200

    FrontEnd:temperature and server status

---
+++ frontend/src/temperature/temperature.service.js
@@ -0,0 +1,27 @@
+(() => {
+	angular.module('kemia-app')
+	.factory('temperatureFactory', temperatureFactory)
+
+	temperatureFactory.$inject = ['$http']
+
+	function temperatureFactory($http) {
+		return {
+			getTemperature: getTemperature
+		}
+
+		function getTemperature() {
+			return $http.get('/gettemperature')
+			.then(getTemperatureComplete)
+			.catch(getTemperatureError)
+		}
+
+		function getTemperatureComplete(response) {
+			console.error(response.data)
+			return response.data
+		}
+
+		function getTemperatureError(error) {
+			console.error('An error occured while getting the current temperature: ', error)
+		}
+	}
+})()
