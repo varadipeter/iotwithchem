8a50ceed0d48b596323666cdb5b267c20b506cc5
Othniel Visky <viskytofi@yahoo.com>
2016-11-13 18:16:53 +0200

    change chart to have one set of data. still dummy data.

--- frontend/src/temperature/temperature.controller.js
+++ frontend/src/temperature/temperature.controller.js
@@ -24,13 +24,13 @@
 		}
 
 		$scope.labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July']
-		$scope.series = ['Series A', 'Series B']
-		$scope.data = [[65, 59, 80, 81, 56, 55, 40], [28, 48, 40, 19, 86, 27, 90]]
+		$scope.series = ['Series A']
+		$scope.data = [[65, 59, 80, 81, 56, 55, 40]]
 
 		$scope.onClick = function (points, evt) {
 			console.log(points, evt)
 		}
-		$scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }]
+		$scope.datasetOverride = [{ yAxisID: 'y-axis-1' }]
 		$scope.options = {
 			scales: {
 				yAxes: [
@@ -39,12 +39,6 @@
 						type: 'linear',
 						display: true,
 						position: 'left'
-					},
-					{
-						id: 'y-axis-2',
-						type: 'linear',
-						display: true,
-						position: 'right'
 					}
 				]
 			}
