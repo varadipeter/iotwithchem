3ab57f7cd60edb032694ce0cf097c407d9e2c5de
Matyas Robert Janos <matyas.robi@yahoo.com>
2016-12-03 11:08:21 +0200

    check the temperature sensor status

--- frontend/src/temperature/temperature.controller.js
+++ frontend/src/temperature/temperature.controller.js
@@ -7,18 +7,38 @@
 	function temperatureController($scope, temperatureFactory,$interval) {
 		let temp=this
 		temp.temperature=0.0
-
+		$scope.placeholder='Request interval'
+		$scope.interval=3
+		let interval=3
+		let refreshId=0
 		getTemperature().then(() => {
 		})
 
-		$interval(getTemperature, 30000)
+		$scope.$watch('interval', function() {
+			$interval.cancel(refreshId)
+			interval = $scope.interval
+			if(interval!=undefined){
+				refreshId=$interval(getTemperature, interval*1000)
+			}
+			console.log(interval)
+			console.log(refreshId)
+		})
+
 
 		function getTemperature() {
 			return temperatureFactory.getTemperature()
 			.then((temperature) => {
+				var oldDate=$scope.temperatureValue
 				temp.temperature = temperature
 				$scope.temperatureValue=temp.temperature.tempvalue
 				$scope.temperatureDate=temp.temperature.tempdate
+				if(oldDate!=temp.temperature.tempvalue){
+					$scope.status=true
+				}
+				else {
+					$scope.status=false
+				}
+
 				return temp.temperature
 			})
 		}
