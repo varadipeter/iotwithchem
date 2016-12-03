caedab10500c91799243183ff33e17466f42ddbb
Matyas Robert Janos <matyas.robi@yahoo.com>
2016-12-03 20:09:41 +0200

    set temperature sensor upload interval from frontend

--- frontend/src/temperature/temperature.controller.js
+++ frontend/src/temperature/temperature.controller.js
@@ -2,45 +2,59 @@
 	angular.module('kemia-app')
 	.controller('temperatureController', temperatureController)
 
-	temperatureController.$inject = ['$scope', 'temperatureFactory','$interval']
+	temperatureController.$inject = ['$scope', 'temperatureFactory','$interval','$window']
 
-	function temperatureController($scope, temperatureFactory,$interval) {
+	function temperatureController($scope, temperatureFactory,$interval,$window) {
 		let temp=this
 		temp.temperature=0.0
-		$scope.placeholder='Request interval'
 		$scope.interval=3
 		let interval=3
 		let refreshId=0
 		getTemperature().then(() => {
 		})
 
-		$scope.$watch('interval', function() {
-			$interval.cancel(refreshId)
-			interval = $scope.interval
-			if(interval!=undefined){
-				refreshId=$interval(getTemperature, interval*1000)
-			}
-			console.log(interval)
-			console.log(refreshId)
-		})
-
-
 		function getTemperature() {
 			return temperatureFactory.getTemperature()
 			.then((temperature) => {
-				var oldDate=$scope.temperatureValue
+				let oldDate=$scope.temperatureDate
+				console.log(temp.temperature)
 				temp.temperature = temperature
 				$scope.temperatureValue=temp.temperature.tempvalue
 				$scope.temperatureDate=temp.temperature.tempdate
-				if(oldDate!=temp.temperature.tempvalue){
-					$scope.status=true
+				if(oldDate!=temp.temperature.tempdate){
+					$scope.status=false
 				}
 				else {
-					$scope.status=false
+					$scope.status=true
 				}
 
 				return temp.temperature
 			})
+		}
+
+		$scope.initTemperatureTime=function() {
+			$interval.cancel(refreshId)
+			interval = $scope.interval
+			setTemperatureTime()
+			refreshId=$interval(getTemperature, interval*1000)
+
+		}
+		$scope.setTemperatureTime=function() {
+			$interval.cancel(refreshId)
+			interval = $scope.interval
+			setTemperatureTime()
+			if (refreshId!=0){
+				$window.alert('Successful')
+			}
+			refreshId=$interval(getTemperature, interval*1000)
+		}
+		function setTemperatureTime() {
+			return temperatureFactory.setTemperatureTime(1, $scope.interval*1000)
+		}
+
+		$scope.setHeaterTemperature=function(){
+			let resp=temperatureFactory.setHeaterTemperature($scope.settedTemperature)
+			console.log(resp)
 		}
 	}
 })()
