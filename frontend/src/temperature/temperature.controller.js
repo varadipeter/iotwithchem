462235fe927eedc98b1427ab6b64afd559cd85d6
Zsebe Akos <zsebea@yahoo.com>
2016-12-05 17:28:29 +0200

    ulimate version

--- frontend/src/temperature/temperature.controller.js
+++ frontend/src/temperature/temperature.controller.js
@@ -8,52 +8,52 @@
 		let temp=this
 		temp.temperature=0.0
 		$scope.interval=3
-		let interval=3
-		let refreshId=0
+		let tempValue=0
+		let tempDate=0
 		getTemperature().then(() => {
 		})
-
+		function refresh(){
+			$scope.temperatureValue=tempValue
+			$scope.temperatureDate=tempDate
+		}
 		function getTemperature() {
 			return temperatureFactory.getTemperature()
 			.then((temperature) => {
 				let oldDate=$scope.temperatureDate
-				console.log(temp.temperature)
 				temp.temperature = temperature
-				$scope.temperatureValue=temp.temperature.tempvalue
-				$scope.temperatureDate=temp.temperature.tempdate
-				if(oldDate!=temp.temperature.tempdate){
+				tempValue=temp.temperature.tempvalue
+				tempDate=temp.temperature.tempdate
+				refresh()
+				console.log(oldDate)
+				console.log(temp.temperature.tempdate-$scope.interval*1000)
+				if(oldDate-$scope.interval*1000>temp.temperature.tempdate){
 					$scope.status=false
 				}
 				else {
 					$scope.status=true
 				}
-
 				return temp.temperature
 			})
 		}
+		$interval(getTemperature, $scope.interval*1000)
 
-		$scope.initTemperatureTime=function() {
-			$interval.cancel(refreshId)
-			interval = $scope.interval
-			setTemperatureTime()
-			refreshId=$interval(getTemperature, interval*1000)
-
-		}
 		$scope.setTemperatureTime=function() {
-			$interval.cancel(refreshId)
-			interval = $scope.interval
+			//$interval.cancel(refreshId)
+			//interval = $scope.interval
 			setTemperatureTime()
-			if (refreshId!=0){
-				$window.alert('Successful')
-			}
-			refreshId=$interval(getTemperature, interval*1000)
+			//if (refreshId!=0){
+			$window.alert('Successful')
+			//}
+			//refreshId=$interval(getTemperature, interval*1000)
 		}
+		
 		function setTemperatureTime() {
 			return temperatureFactory.setTemperatureTime(1, $scope.interval*1000)
 		}
 
 		$scope.setHeaterTemperature=function(){
 			let resp=temperatureFactory.setHeaterTemperature($scope.settedTemperature)
+			$window.alert('Successful')
 			console.log(resp)
 		}
 	}
